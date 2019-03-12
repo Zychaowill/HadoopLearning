d12a443d5d9e4de354d597d9b12e3e3cd34e5a9c
Zychaowill <15536542139@163.com>
2019-03-12 11:48:25 +0800

    query hive log

--- src/main/java/hive/deepin/HiveStatementExample.java
+++ src/main/java/hive/deepin/HiveStatementExample.java
@@ -1,10 +1,15 @@
 package hive.deepin;
 
+import com.google.common.collect.Queues;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.apache.hive.jdbc.HiveStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author buildupchao
@@ -15,11 +20,21 @@ public class HiveStatementExample {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HiveStatementExample.class);
 
+    private static final ExecutorService HIVE_LOG_HANLDERS = new ThreadPoolExecutor(
+            5,
+            5,
+            0,
+            TimeUnit.SECONDS,
+            Queues.newArrayBlockingQueue(5),
+            new BasicThreadFactory.Builder().namingPattern("hive-log-handler-%d").build(),
+            new ThreadPoolExecutor.AbortPolicy()
+    );
+
     private Connection connection;
 
     private void init() {
         String driver = "org.apache.hive.jdbc.HiveDriver";
-        String url = "jdbc:hive2://master:10000/practice3?auth=noSasl";
+        String url = "jdbc:hive2://master:10000/db1?auth=noSasl";
         String user = "jangz";
         String password = "jangz";
         try {
@@ -35,19 +50,71 @@ public class HiveStatementExample {
     public void getApplicationId() throws SQLException {
         init();
 
-        HiveStatement statement = (HiveStatement) connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from employees");
+        HiveStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = (HiveStatement) connection.createStatement();
+            System.out.println("statement: " + statement.toString());
+            HIVE_LOG_HANLDERS.execute(new HiveLogHandler(statement));
+            resultSet = statement.executeQuery("select count(*) from record");
+            System.out.println("DONE>>>>>>>>>>>");
+        } finally {
+            close(connection, statement, resultSet);
+            HIVE_LOG_HANLDERS.shutdown();
+        }
+    }
 
-        String keywords = "Submitted application";
-        String yarnApplicationId = new String();
-        System.out.println(statement.getQueryLog());
-        for (String queryLog : statement.getQueryLog()) {
-            if (queryLog.contains(keywords)) {
-                yarnApplicationId = queryLog.substring(queryLog.indexOf(keywords) + keywords.length(), queryLog.length() - 1);
-                break;
+    public void close(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            if (resultSet != null && !resultSet.isClosed()) {
+                resultSet.close();
+            }
+            if (statement != null && !statement.isClosed()) {
+                statement.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            LOGGER.error("Close resource error!");
+        }
+    }
+
+    static class HiveLogHandler implements Runnable {
+
+        private HiveStatement hiveStatement;
+
+        public HiveLogHandler(HiveStatement hiveStatement) {
+            this.hiveStatement = hiveStatement;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String keywords = "application_";
+                String yarnApplicationId = new String();
+
+                while (hiveStatement.hasMoreLogs()) {
+                    for (String queryLog : hiveStatement.getQueryLog(true, 100)) {
+//                            if (queryLog.contains(keywords)) {
+                        yarnApplicationId = queryLog;
+//                            yarnApplicationId = queryLog.substring(queryLog.indexOf(keywords) + keywords.length(), queryLog.length() - 1);
+
+                        System.out.println(queryLog);
+                        break;
+//                            }
+                    }
+                    try {
+                        Thread.sleep(1000L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+//               LOGGER.info("YARN APPLICATION ID: [{}]", yarnApplicationId);
+            } catch (SQLException ex) {
+                LOGGER.info("Cannot get YARN APPLICATION ID.");
             }
         }
-        LOGGER.info("YARN APPLICATION ID: [{}]", yarnApplicationId);
     }
 
     public static void main(String[] args) throws SQLException {
